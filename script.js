baa9d52cee20b13d7cb6ea848e7351e95c7d0e19
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-02-09 14:45:52 +0000

    fixed

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const menuIcon = document.querySelector(".menu-icon");
-const sideMenu = document.querySelector(".side-menu");
-const menuItems = document.querySelectorAll(".button");
-const animatedElements = document.querySelectorAll(".animate-on-scroll");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const sideMenu = document.querySelector(".side-menu");
+  const menuItems = document.querySelectorAll(".button");
+  const animatedElements = document.querySelectorAll(".animate-on-scroll");
 
-menuIcon.addEventListener("click", function () {
-  sideMenu.classList.toggle("show");
-});
-
-menuItems.forEach((element) => {
-  element.addEventListener("click", () => {
-    sideMenu.classList.remove("show");
+  menuIcon.addEventListener("click", function () {
+    sideMenu.classList.toggle("show");
   });
-});
 
-function handleScroll() {
+  menuItems.forEach((element) => {
+    element.addEventListener("click", () => {
+      sideMenu.classList.remove("show");
+    });
+  });
+
+  function handleScroll() {
+    animatedElements.forEach((element) => {
+      const rect = element.getBoundingClientRect();
+      const isVisible = rect.top < window.innerHeight * 0.8 && rect.bottom > 0;
+
+      if (isVisible) {
+        element.classList.add("visible");
+        element.classList.remove("hidden");
+      } else {
+        element.classList.remove("visible");
+        element.classList.add("hidden");
+      }
+    });
+  }
+
   animatedElements.forEach((element) => {
-    const rect = element.getBoundingClientRect();
-    const isVisible = rect.top < window.innerHeight * 0.8 && rect.bottom > 0;
-
-    if (isVisible) {
-      element.classList.add("visible");
-      element.classList.remove("hidden");
-    } else {
-      element.classList.remove("visible");
-      element.classList.add("hidden");
-    }
+    element.classList.add("hidden");
   });
-}
 
-document.addEventListener("scroll", handleScroll);
-handleScroll();
+  document.addEventListener("scroll", handleScroll);
+  handleScroll();
+});
